ad215d4ce3b8952cf7aced7cd966d0a36f385e6c
al-chari <butchillas@protonmail.com>
2026-03-26 10:17:26 -0700

    Trouble shooting, currently working on merging the raw data. Most attempts have been removed to clear the file. Prefixes was made as a possible way to recognise the different categories to separate the different cell lines by R+int and _ however that did not work as well as _. The grr is not only the file paths in both attemps but also xyz has been modified from earlier the attempt was to merge all of them by also adding lapplym, however because there was a second plasmid I am going to make the function separate between the plasmid, next plan is to make a loop for exactly that it will search for each plasmid and make a list of the names, then I will try and reshape it to make it more generic as well as using the mean for identical cell lines
    Everything has been extracted and renamed, originally each tsv has a numeric name, those names were replaced with the compressed fine name which includes the cell lines and replicants.

--- Code/Import datasets.R
+++ Code/Import datasets.R
@@ -2,11 +2,45 @@
 #from the sanger format into a format that works for our datasets
 #It will not import all data but rather it will use
 library(dplyr)
+library(tidyverse)
 #the function for importing the data, descriptions of what everything means will be below
 tttt<-list(C2BBe1_c903R1.read_count, C2BBe1_c903R2.read_count, C2BBe1_c903R3.read_count)
-Reduce(function(x, y)
-  merge(x, y, by = c('sgRNA', 'gene', 'ERS717283.plasmid') ,all = TRUE), dataset)
+patr<-"/Users/user/Documents/CRISPR_thesis/gRNA Sanger"
+grr<-list.files(path=patr, full.names = TRUE)
+grr
+reduce(full_join, by = c('sgRNA', 'gene', 'ERS717283.plasmid'))
+
+grr <- list.files(path = patr, pattern = "\\.tsv$", full.names = TRUE)
+
+file.info("/Users/user/Documents/CRISPR_thesis/gRNA Sanger")
+
+ggggg<-basename(grr, read.tsv)
+ggggg
+
+prefixes <- str_extract(ggggg, "R\\d+(?=[._]|$)")
+prefixes
+# Step 1: Read all files into a list
+file_list <- lapply(grr, read.delim)
+
+# Step 2: Get column names of each file
+all_colnames <- lapply(file_list, colnames)
+all_colnames
+
+xyz
+
+xyz<-Reduce(function(x, y)
+  merge(x, y, by = c('sgRNA', 'gene') ,all = TRUE),
+  lapply(grr, read.delim))
+colnames(xyz)
+
+
 data<-(C2BBe1_c903R1.read_count)
+#Here I am writing the normalisation
+importfunct<- function(file_path, dataset) {
+  #path<-
+}
+
+
 Imports <- function(types,  dataset) {
   
   #The type will define if the data will be imported from Achilles or from Sanger and thus what format it will use
@@ -30,5 +64,5 @@ Imports <- function(types,  dataset) {
   #or a complete folder, name will be the the name of this file or folder
   
 }
-
+##FDR P values
            
